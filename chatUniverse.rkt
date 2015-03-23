28b1c0e5c49adfeb86daf9a05ec2881cd279be99
Liam McDonald <liam.mcdonald000@gmail.com>
2015-03-23 17:14:25 -0500

    changes
    to server

--- chatUniverse.rkt
+++ chatUniverse.rkt
@@ -4,20 +4,30 @@
 (require 2htdp/universe)
 (require picturing-programs)
 
-(define-struct verse (state all msgs))
+(define-struct verse (all msgs))
 
 (define (add-verse verse world)
-  (make-verse (verse-state verse)
-              (cons world (verse-all verse))
+  (make-verse (cons world (verse-all verse))
               (verse-msgs verse)))
 
 (define (add-msg verse msg)
-  (make-verse (verse-state verse)
-              (verse-all verse)
+  (make-verse (verse-all verse)
               (cons msg (verse-msgs verse))))
 
-(check-expect (add-verse (make-verse 'active (list "d" "l" "c")(list "Hi" "Wow" "Oh")) "j")
-              (make-verse 'active (list "j" "d" "l" "c")(list "Hi" "Wow" "Oh")))
+(check-expect (add-verse (make-verse (list "d" "l" "c")(list "Hi" "Wow" "Oh")) "j")
+              (make-verse (list "j" "d" "l" "c")(list "Hi" "Wow" "Oh")))
 
-(check-expect (add-msg (make-verse 'active (list "d" "l" "c")(list "Hi" "Wow" "Oh")) "Hello")
-              (make-verse 'active (list "d" "l" "c")(list "Hello" "Hi" "Wow" "Oh")))
+(check-expect (add-msg (make-verse (list "d" "l" "c")(list "Hi" "Wow" "Oh")) "Hello")
+              (make-verse (list "d" "l" "c")(list "Hello" "Hi" "Wow" "Oh")))
+
+(define (new-world verse world)
+  (make-bundle (add-verse (add-msg verse (string-append (iworld-name world) " has entered the chat.")) world)
+               (verse-msgs(add-msg verse (string-append (iworld-name world) " has entered the chat.")))))
+
+(check-expect (new-world (make-verse (list "a" "b" "c")
+                                     (list "hi" "boo" "why"))
+                         iworld1)
+              (make-bundle (make-verse (list "iworld" "a" "b" "c")
+                                       (list "iworld has entered the chat" "hi" "boo" "why"))
+                           (list "iworld has entered the chat" "hi" "boo" "why")
+                           (list "iworld" "a" "b" "c")))
